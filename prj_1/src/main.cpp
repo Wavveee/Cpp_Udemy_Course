9c383cf548f5ce0afd4636d0f1fa8734be68a5b7
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2025-02-12 19:52:14 +0200

    --

---
+++ prj_1/src/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int favNumb;
+
+    cout << "enter your favorite number between 1 and 100 ";
+        cin >> favNumb;
+
+    cout << "amazing is my favorite number too" << endl;
+    cout << "no really " << favNumb << " is my favorite number" << endl;
+        return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "cmd line check" <<endl;
+return 0;
+}
